9116a8611b0b7c0e9bfdea576ad7cec813ea3c56
Pamela Iupi Peixinho <PamelaPeixinho@MacBook-Pro-de-Pamela.local>
2016-05-16 10:08:29 -0300

    Add CargaMotor e TempLiquidoArrefecimento

---
+++ CarSync/app/src/main/java/br/com/fei/carsync/manager/model/CargaMotor.java
@@ -0,0 +1,73 @@
+package br.com.fei.carsync.manager.model;
+
+import android.util.Log;
+
+import br.com.fei.carsync.view.activity.Comunicacao;
+import br.com.fei.carsync.view.activity.Requisicoes;
+
+/**
+ * Created by PamelaPeixinho on 5/6/16.
+ */
+public class CargaMotor extends AbstractComandoOBD {
+
+    public CargaMotor() {
+        super("0104");
+    }
+
+    @Override
+    public void run() {
+//        -----------------
+//        setRunning(true);
+//        while(!this.isInterrupted()){
+//        while(isRunning()){
+//        -----------------
+
+        while(true){
+            Log.d("Thread", "Carga Motor");
+            setResposta(Comunicacao.sendReceiveOBD(getPID())); //synchronized function
+            calculate();
+            Requisicoes.respCargaMotor = getResposta();
+            Requisicoes.updateRequisicoesView();
+            Log.d("TAG", "Resp = " + getResposta());
+            try {
+                sleep(1000);
+                if (isSuspend()) {
+                    synchronized(this) {
+                        Log.d("TAG", "Carga Motor");
+                        while(isSuspend())
+                            wait();
+                        Log.d("TAG", "Sai, não estou mais suspenso Carga Motor");
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //Log.d("Velocidade: ", getResposta());
+        }
+
+//        Log.d("Thread", "SAI DO LOOP DE REQUISICOES DA VELOCIDADE");
+//        this.currentThread().interrupt();
+//        Log.d("Thread", "isInterrupted?" + String.valueOf(this.currentThread().isInterrupted()));
+    }
+
+    @Override
+    protected void calculate() {
+        String respAux = getResposta();
+        if (!respAux.substring(0,2).equals("41"))
+            return;
+        setResposta(respAux);
+        respAux = respAux.substring(respAux.length() - 2);
+        Long i = Long.parseLong(respAux, 16);
+        i*= (100/255);
+        Log.d("Thread", "Resp CargaMotor Dec: " + String.valueOf(i));
+        setResposta(Long.toString(i));
+    }
+
+    @Override
+    public synchronized void notifyThread() {
+        if (!isSuspend()){
+            notify();
+        }
+    }
+}
---
+++ CarSync/app/src/main/java/br/com/fei/carsync/manager/model/TemperaturaLiquidoArrefecimento.java
@@ -0,0 +1,64 @@
+package br.com.fei.carsync.manager.model;
+
+import android.util.Log;
+
+import br.com.fei.carsync.view.activity.Comunicacao;
+import br.com.fei.carsync.view.activity.Requisicoes;
+
+/**
+ * Created by PamelaPeixinho on 5/9/16.
+ */
+    public class TemperaturaLiquidoArrefecimento extends AbstractComandoOBD {
+
+            public TemperaturaLiquidoArrefecimento(){ super("0105"); }
+
+            @Override
+            public void run() {
+    //        setSuspend(false);
+                while(true){
+                    setResposta(Comunicacao.sendReceiveOBD(getPID())); //synchronized function
+                    calculate();
+                    Requisicoes.respTemperaturaLiquidoArrefecimento = getResposta();
+                    Requisicoes.updateRequisicoesView();
+                    Log.d("Thread", "TempLiquidoArrefecimento");
+                    try {
+                        Thread.sleep(1000);
+                        if (isSuspend()) {
+                            synchronized(this) {
+                                Log.d("Thread", "Suspender TempLiquidoArrefecimento");
+                                while(isSuspend())
+                                    wait();
+                                Log.d("Thread", "Sai, não estou mais suspenso TempLiquidoArrefecimento");
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+    //            Log.d("RPM ", getResposta());
+                }
+
+    //        Log.d("Thread", "SAI DO LOOP DE REQUISICOES DO RPM");
+    //        this.currentThread().interrupt();
+    //        Log.d("Thread", "isInterrupted?" + String.valueOf(this.currentThread().isInterrupted()));
+            }
+
+            @Override
+            protected void calculate() {
+                String respAux = getResposta();
+                if (!respAux.substring(0,2).equals("41"))
+                    return;
+                respAux = respAux.substring(respAux.length() - 2);
+                Long i = Long.parseLong(respAux, 16);
+                i-=40;
+                Log.d("Thread", "Resp TempLiquidoArrefecimento Dec: " + String.valueOf(i));
+                setResposta(Long.toString(i));
+
+            }
+
+            @Override
+            public synchronized void notifyThread(){
+                if (!isSuspend())
+                    notify();
+            }
+        }
+
